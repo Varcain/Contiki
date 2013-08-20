3ba2a11eead2623fbefd123e9cced955dc541b27
Kamil Lulko <rev13@wp.pl>
2013-08-20 20:28:07 +0200

    Fixes for stm32f100 cpu

--- cpu/stm32f100/script.sh
+++ cpu/stm32f100/script.sh
@@ -1,5 +1,4 @@
 # stop remaining openocd instances
 killall -9 openocd
 # start new openocd and reset and init chip
-#/media/data/openOCD/bin/openocd -f board/stm32vldiscovery.cfg -c init -c"reset init" -c"poll"
-/media/data/openocd-git/openocd/bin/openocd -f board/stm32vldiscovery.cfg -c init -c"reset init" -c"poll"
+/home/limak/bin/openocd -f board/stm32vldiscovery.cfg -c init -c"reset init" -c"poll"
